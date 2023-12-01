0aab67c3f7f38e8a080c51d66ca84ef40c70a0e3
03Pavi <pavitar.1127@zenmonk.tech>
2023-12-01 17:10:47 +0530

    /reactts-folder-structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "@/pages/homepage/components/header";
-import RoutesManager from "./setup/routes-manager";
+import RoutesManager from "./setup/routes";
 const App = () => {
   return <RoutesManager />;
 };
